ab1c3e7946c40f20dc19d0a3fb15dbae48513c81
MehmetKaya <mkaya.us@gmail.com>
2020-07-31 14:31:47 -0400

    Changes

--- src/test/java/utils/BrowserUtils.java
+++ src/test/java/utils/BrowserUtils.java
@@ -13,6 +13,8 @@ import java.util.function.Function;
 
 public class BrowserUtils {
 
+    //Olddd
+
     //It will be used to pause our test execution
     //just provide number of seconds as a parameter
     public static void wait(int seconds) {
